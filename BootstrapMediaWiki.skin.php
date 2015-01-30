6fb757899347ff1f1c3f16036415129eb234cc0d
Laurent Pouilloux <lolo@pouilloux.org>
2015-01-30 10:36:52 +0100

    Suppression de l'image fixe, car beaucoup plus simple si intégrée dans la page Bootstrap:Footer

--- BootstrapMediaWiki.skin.php
+++ BootstrapMediaWiki.skin.php
@@ -241,10 +241,6 @@ class BootstrapMediaWikiTemplate extends QuickTemplate {
 		<div class="bottom">
 			<div class="container"> 
 				<?php $this->includePage('Bootstrap:Footer'); ?></br>
-				
-				<p style="text-align:center">
-				<img width="100%" src='/mediawiki/skins/bootstrap-mediawiki-g5k/images/g5k_fundings.svg' alt='Logo'/>
-				</p> 
 				<footer> 
 					<p>&copy; <?php echo date('Y'); ?> by <a href="<?php echo (isset($wgCopyrightLink) ? $wgCopyrightLink : 'http://borkweb.com'); ?>"><?php echo (isset($wgCopyright) ? $wgCopyright : 'BorkWeb'); ?></a> 
 						&bull; Powered by <a href="http://mediawiki.org">MediaWiki</a> 
